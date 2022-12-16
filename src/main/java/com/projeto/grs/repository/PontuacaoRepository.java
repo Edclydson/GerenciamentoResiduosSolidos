030e45729b3cb5bc21d7c4f4c95992cedd4ec3a3
Edclydson <edclydson.sousa@gmail.com>
2022-12-16 13:48:11 -0300

    :sparkles: feat: Adicionando busca pelo id

--- src/main/java/com/projeto/grs/repository/PontuacaoRepository.java
+++ src/main/java/com/projeto/grs/repository/PontuacaoRepository.java
@@ -4,6 +4,9 @@ import com.projeto.grs.modelo.Pontuacao;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PontuacaoRepository extends CrudRepository <Pontuacao, String>{
+    Optional<Pontuacao> findById(String cpf);
 }
