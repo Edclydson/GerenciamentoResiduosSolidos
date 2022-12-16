33c33a525fdf11740d284bd47c313d4db7d895fd
Edclydson <edclydson.sousa@gmail.com>
2022-12-16 13:56:53 -0300

    :sparkles: feat: Criação do data transfer object de produto

---
+++ src/main/java/com/projeto/grs/dto/ProdutoDTO.java
@@ -0,0 +1,12 @@
+package com.projeto.grs.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoDTO{
+    private Double valorPontos;
+    private String nomeProduto;
+    private int quantidadeProduto;
+}
