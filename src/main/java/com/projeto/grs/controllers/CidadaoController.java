7cee23f77567e898ba42712540d0e33fd4bb3712
Edclydson <edclydson.sousa@gmail.com>
2022-12-16 14:14:58 -0300

    :sparkles: feat: Adicionando novos endpoints(Resgate de produto e Consulta de Pontuação)

--- src/main/java/com/projeto/grs/controllers/CidadaoController.java
+++ src/main/java/com/projeto/grs/controllers/CidadaoController.java
@@ -1,15 +1,18 @@
 package com.projeto.grs.controllers;
 
 import com.projeto.grs.dto.CidadaoDTO;
-import com.projeto.grs.modelo.Cidadao;
+import com.projeto.grs.dto.ProdutoDTO;
+import com.projeto.grs.modelo.Pontuacao;
 import com.projeto.grs.modelo.Produto;
 import com.projeto.grs.services.CidadaoService;
 import com.projeto.grs.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/dashboard")
@@ -20,19 +23,31 @@ public class CidadaoController{
     @Autowired
     CidadaoService cidadaoService;
 
+
     @PreAuthorize(value = "hasRole('USER')")
     @GetMapping("/produtos")
-    public List<Produto> listarProdutos(){
+    public ResponseEntity<Iterable<Produto>> listarProdutos(){
        return produtoService.listar();
     }
 
-    //TEMPORARIO
-    @PreAuthorize(value = "hasRole('ADMIN')")
-    @GetMapping("/cidadoes")
-    public List<Cidadao> listarCidadaos(){
-        return cidadaoService.listarCidadaos();
+    //ENDPOINT SERÁ REALOCADO PARA OUTRA API
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/produto/novo")
+    public void novoProduto(@RequestBody ProdutoDTO novoProduto){
+        produtoService.novoProduto(novoProduto);
+    }
+    @PutMapping("/resgatar/{cpf}/{ref}")
+    public ResponseEntity resgateProduto(@PathVariable("cpf") String cpf, @PathVariable("ref") String referenciaProduto){
+        return cidadaoService.resgastar(cpf,referenciaProduto);
     }
 
+    @PreAuthorize(value = "hasRole('USER')")
+    @GetMapping("/pontuacao/{id}")
+    public Optional<Pontuacao> pontosDoCidadao(@PathVariable("id") String id){
+        return cidadaoService.pontosDoCidadao(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/cadastro")
     public void novoCidadao(@RequestBody CidadaoDTO dto){
         cidadaoService.cadastro(dto);
