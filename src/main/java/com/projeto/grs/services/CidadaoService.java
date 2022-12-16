e34aa27d98e7c20c28ab8c5a35a19009cc118018
Edclydson <edclydson.sousa@gmail.com>
2022-12-16 13:05:14 -0300

    :sparkles: feat: Adicionando funcionalidade de Resgate do Produto, Consulta de Pontos, Atualização e Verificação de Pontos

--- src/main/java/com/projeto/grs/services/CidadaoService.java
+++ src/main/java/com/projeto/grs/services/CidadaoService.java
@@ -3,23 +3,29 @@ package com.projeto.grs.services;
 import com.projeto.grs.dto.CidadaoDTO;
 import com.projeto.grs.modelo.Cidadao;
 import com.projeto.grs.modelo.Pontuacao;
+import com.projeto.grs.modelo.Produto;
 import com.projeto.grs.modelo.Roles;
 import com.projeto.grs.repository.CidadaoRepository;
 import com.projeto.grs.repository.PontuacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
-public class CidadaoService{
+public class CidadaoService {
     @Autowired
     private CidadaoRepository cidadaoRepository;
 
     @Autowired
     private PontuacaoRepository pontuacaoRepository;
+
+    @Autowired
+    private ProdutoService produtoService;
 
 
     public void cadastro(CidadaoDTO dto){
@@ -45,11 +51,53 @@ public class CidadaoService{
         catch (Exception e){e.printStackTrace();}
     }
 
-    public List<Cidadao> listarCidadaos(){
-        return cidadaoRepository.findAll();
+    public Optional<Pontuacao> pontosDoCidadao(String id){
+        return pontuacaoRepository.findById(id);
     }
 
+    public ResponseEntity resgastar(String cpf, String referencia){
+        Pontuacao pontuacaoCidadao = pontosDoCidadao(cpf).get();
+        Produto produtoResgatar = produtoService.consultarProduto(referencia).get();
+        if(verificaPontosCidadao(cpf) == true){
+            if (verificaPontosParaSaque(pontuacaoCidadao,produtoResgatar) == true){
+                try{
+                    boolean processoResgateProduto = produtoService.resgateUmProduto(produtoResgatar.getIdProduto().toString());
+                    if(processoResgateProduto == true){
+                        atualizaPontosCidadao(pontuacaoCidadao.getIdPontos(), produtoResgatar.getValorPontos());
+                        return ResponseEntity.ok("Saque realizado com sucesso!");
+                    }else{
+                        return ResponseEntity.ok("Saque não realizado! Produto indísponível");
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return ResponseEntity.ok("Deu ruim! Você não tem pontos suficiente para sacar este produto! :(");
+    }
     private BCryptPasswordEncoder encoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    private void atualizaPontosCidadao(String cpf,Double pontosSaque){
+        try{
+            Pontuacao novaPontuacao = pontuacaoRepository.findById(cpf).get();
+            novaPontuacao.setPontos(novaPontuacao.getPontos() - pontosSaque);
+            pontuacaoRepository.save(novaPontuacao);
+        }catch (Exception e){e.printStackTrace();}
+    }
+
+    private boolean verificaPontosCidadao(String cpf){
+        Pontuacao pontosCidadao = pontuacaoRepository.findById(cpf).get();
+        if(pontosCidadao.getPontos() > 0){
+            return true;
+        }
+        return false;
+    }
+    private boolean verificaPontosParaSaque(Pontuacao pontosCidadao,Produto produtoAsacar){
+        if(pontosCidadao.getPontos() >= produtoAsacar.getValorPontos()){
+            return true;
+        }
+        return false;
     }
 }
