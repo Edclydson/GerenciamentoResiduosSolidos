c7ef62a78e6f5ec24bc9acbb6c758af99284f72c
Edclydson <edclydson.sousa@gmail.com>
2023-01-24 19:20:57 -0300

    :recycle: refactor: Atualização da configuração da documentação para o Swagger 3

--- src/main/java/com/projeto/grs/config/SwaggerConfig.java
+++ src/main/java/com/projeto/grs/config/SwaggerConfig.java
@@ -1,35 +1,27 @@
 package com.projeto.grs.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.hibernate.testing.orm.domain.contacts.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+
 
 @Configuration
 public class SwaggerConfig {
 
-
     @Bean
-    public Docket docket(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(informacoesDaApi())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.projeto.grs.controllers"))
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API Gerenciamento Residuos Solidos")
+                        .description("Api desenvolvida com base no projeto apresentado no Jovens Talentos do Juventude Digital")
+                        .version("1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Github")
+                        .url("https://github.com/edclydson"));
     }
 
-    private ApiInfo informacoesDaApi() {
-        return new ApiInfoBuilder()
-                .title("API Gerenciamento Residuos Solidos")
-                .description("Api desenvolvida com base no projeto apresentado no Jovens Talentos do Juventude Digital \n Projeto disponível em: https://github.com/Edclydson")
-                .contact(new Contact("Edclydson Sousa","https://linkedin.com/in/edclydson","edclydson.sousa@gmail.com"))
-                .version("1.0.0")
-                .build();
-    }
 }
