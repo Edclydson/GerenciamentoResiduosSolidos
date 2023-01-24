bdb26065d1336172251ef0b852d0f21c24934fbf
Edclydson <edclydson.sousa@gmail.com>
2023-01-24 19:10:56 -0300

    :recycle: refactor: Add Annotations do lombok e implementações do UserDetails

--- src/main/java/com/projeto/grs/modelo/Cidadao.java
+++ src/main/java/com/projeto/grs/modelo/Cidadao.java
@@ -1,12 +1,16 @@
 package com.projeto.grs.modelo;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.*;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
@@ -14,29 +18,33 @@ import java.util.stream.Collectors;
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "cidadaos")
 public class Cidadao implements UserDetails{
 
     @Id
     @Column(length = 11)
+    @CPF
     private String cpf;
 
     private String nome;
     private String idade;
+    @Email
     private String email;
     private String telefone;
     private String endereco;
     private String senha;
 
+    private String roles;
+
     @OneToOne
     private Pontuacao pontos;
 
 
-    private String role;
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.stream(getRole()
+        return Arrays.stream(getRoles()
                 .split(","))
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
