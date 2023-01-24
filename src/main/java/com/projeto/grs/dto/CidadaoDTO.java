98a3d958597978dc1d7603ca3d85cf701de8f2e4
Edclydson <edclydson.sousa@gmail.com>
2023-01-24 19:06:39 -0300

    :sparkles: Add toString no DTO

--- src/main/java/com/projeto/grs/dto/CidadaoDTO.java
+++ src/main/java/com/projeto/grs/dto/CidadaoDTO.java
@@ -3,13 +3,27 @@ package com.projeto.grs.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 
 @Getter
 @Setter
 public class CidadaoDTO{
+    @Override
+    public String toString() {
+        return "CidadaoDTO{" +
+                "nome='" + nome + '\'' +
+                ", idade='" + idade + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", endereco='" + endereco + '\'' +
+                '}';
+    }
 
     private String nome;
     private String idade;
+    @CPF
     private String cpf;
     private String email;
     private String senha;
