8eb51a76323dafb58537e8d88096c35b8584c35d
Edclydson <edclydson.sousa@gmail.com>
2023-01-25 15:41:18 -0300

    :recycle: refactor: Alterando dependencias e os métodos do controller

--- src/main/java/com/projeto/grs/controllers/CidadaoController.java
+++ src/main/java/com/projeto/grs/controllers/CidadaoController.java
@@ -2,55 +2,63 @@ package com.projeto.grs.controllers;
 
 import com.projeto.grs.dto.CidadaoDTO;
 import com.projeto.grs.dto.ProdutoDTO;
-import com.projeto.grs.modelo.Pontuacao;
 import com.projeto.grs.modelo.Produto;
-import com.projeto.grs.services.CidadaoService;
-import com.projeto.grs.services.ProdutoService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.projeto.grs.services.citizen.CitizenActions;
+import com.projeto.grs.services.citizen.CitizenService;
+import com.projeto.grs.services.product.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping("/dashboard")
 public class CidadaoController{
-    @Autowired
-    ProdutoService produtoService;
+    final ProductService productService;
+    final CitizenActions citizenActions;
+    final CitizenService citizenService;
 
-    @Autowired
-    CidadaoService cidadaoService;
+    public CidadaoController(ProductService productService,
+                             CitizenActions citizenActions,
+                             CitizenService citizenService) {
+        this.productService = productService;
+        this.citizenActions = citizenActions;
+        this.citizenService = citizenService;
+    }
 
 
-    @PreAuthorize(value = "hasRole('USER')")
     @GetMapping("/produtos")
-    public ResponseEntity<Iterable<Produto>> listarProdutos(){
-       return produtoService.listar();
+    public ResponseEntity<List<Produto>> listarProdutos(){
+       return productService.listar();
     }
 
     //ENDPOINT SERÁ REALOCADO PARA OUTRA API
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/produto/novo")
     public void novoProduto(@RequestBody ProdutoDTO novoProduto){
-        produtoService.novoProduto(novoProduto);
+        productService.novoProduto(novoProduto);
     }
+    
     @PutMapping("/resgatar/{cpf}/{ref}")
     public ResponseEntity resgateProduto(@PathVariable("cpf") String cpf, @PathVariable("ref") String referenciaProduto){
-        return cidadaoService.resgastar(cpf,referenciaProduto);
+        return citizenActions.resgastar(cpf,referenciaProduto);
     }
 
-    @PreAuthorize(value = "hasRole('USER')")
     @GetMapping("/pontuacao/{id}")
-    public Optional<Pontuacao> pontosDoCidadao(@PathVariable("id") String id){
-        return cidadaoService.pontosDoCidadao(id);
+    public ResponseEntity pontosDoCidadao(@PathVariable("id") String id){
+        if(citizenActions.pontosDoCidadao(id).isPresent()){
+            return ResponseEntity.ok(citizenActions.pontosDoCidadao(id));
+        }
+        return ResponseEntity.ok("Cidadão não consta na base de dados!");
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/cadastro")
-    public void novoCidadao(@RequestBody CidadaoDTO dto){
-        cidadaoService.cadastro(dto);
+    public ResponseEntity novoCidadao(@RequestBody @Valid CidadaoDTO dto, UriComponentsBuilder uriBuilder){
+        var uri = uriBuilder.path("/cadastro").build(dto);
+        return citizenService.cadastro(dto,uri);
     }
 
 }
